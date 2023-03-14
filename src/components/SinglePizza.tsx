03c04365f5c08885a5a13e570618b14e2efe271a
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-03-14 16:54:07 +0300

    add react icons

--- src/components/SinglePizza.tsx
+++ src/components/SinglePizza.tsx
@@ -1,18 +1,22 @@
 import React, {FC} from 'react'
 import Pizza from '../modals/Pizza'
 
-interface DisplayPizzasProps {
-  pizzasList: Pizza[]
+interface SinglePizzaProps {
+  pizza: Pizza
 }
 
-const DisplayPizzas: FC<DisplayPizzasProps> = ({pizzasList}) => {
+const SinglePizza: FC<SinglePizzaProps> = ({ pizza }) => {
   return (
-    <div className='container'>
-      {pizzasList.map((pizza) => {
-        return pizza.title
-      })}
+    <div className='pizza'>
+      <img src={`/images/${pizza.img}`} alt={pizza.title} />
+      <h2>{pizza.title}</h2>
+      <span>{pizza.price} ₽</span>
+
+      <div className='pizza-controls'>
+        
+      </div>
     </div>
   )
 }
 
-export default DisplayPizzas
+export default SinglePizza
